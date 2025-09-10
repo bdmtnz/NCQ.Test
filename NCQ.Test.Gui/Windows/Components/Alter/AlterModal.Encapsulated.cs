bd110fffa96f2191805472e96426bf19a265339c
bdmtnz <martinez_o@hotmail.es>
2025-09-09 19:25:55 -0500

    feat: add mapping config

---
+++ NCQ.Test.Gui/Windows/Components/Alter/AlterModal.Encapsulated.cs
@@ -0,0 +1,22 @@
+﻿using Mapster;
+using NCQ.Test.Domain.Tasks;
+using NCQ.Test.Gui.Domain.Common.Contracts.Gui;
+
+namespace NCQ.Test.Gui.Windows.Components.Alter
+{
+    public partial class AlterModal : IValueModal<Task>
+    {
+        public Task GetValue()
+        {
+            var value = FormMvvm.GetViewModel<AlterModalViewModel>();
+            return value.Adapt<Task>();
+        }
+
+        public bool IsValid()
+        {
+            var value = FormMvvm.GetViewModel<AlterModalViewModel>();
+            var result = new AlterModalValidator().Validate(value);
+            return result.IsValid;
+        }
+    }
+}
